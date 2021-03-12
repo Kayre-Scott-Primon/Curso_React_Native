908acbac69dd2b2a6a850f1d05d21a8fafb51a89
Kayre-Scott-Primon <kayrescott@gmail.com>
2021-03-12 00:24:14 -0300

    atualização

--- Projects/tasks-backend/api/user.js
+++ Projects/tasks-backend/api/user.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt-nodejs')
 
 module.exports = app => {
      const obterHash = (password, callback) => {
-          bcrypt.getSalt(10, (err, salt) => {
+          bcrypt.genSalt(10, (err, salt) => {
                bcrypt.hash(password, salt, null, (err, hash) => callback(hash))
           })
      }
