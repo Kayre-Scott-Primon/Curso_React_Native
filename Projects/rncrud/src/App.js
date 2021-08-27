33b7d072426f7ca0bfa3ff9982e1583eea78ae10
Kayre-Scott-Primon <kayrescott@gmail.com>
2021-08-27 08:14:43 -0300

    add fluxo

---
+++ Projects/rncrud/src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import UserList from './views/UserList';
+import UserForm from './views/UserForm';
+import { Button } from 'react-native-elements/dist/buttons/Button';
+import { Icon } from 'react-native-elements';
+
+const Stack = createNativeStackNavigator()
+
+const App = props => {
+  return (
+     <NavigationContainer>
+          <Stack.Navigator
+               initialRouteName='UserList'
+               screenOptions={screenOptions}
+          >
+               <Stack.Screen
+                    name='UserList'
+                    component={UserList}
+                    options={({ navigation }) => {
+                         return {
+                              title: 'Lista de Usuarios',
+                              headerRight: () => (
+                                   <Button
+                                        onPress={() => navigation.navigate('UserForm')}
+                                        type='clear'
+                                        icon={<Icon name='add' size={25} color={'#fff'}/>}
+                                   />
+                              )
+                         }
+                    }}
+               />
+               <Stack.Screen
+                    name='UserForm'
+                    component={UserForm}
+                    options={{
+                         title: 'Formulario de Usuarios'
+                    }}
+               />
+          </Stack.Navigator>
+     </NavigationContainer>
+  )
+}
+
+const screenOptions = {
+     headerStyle: {
+          backgroundColor: '#f4511e'
+     },
+     headerTintColor: '#fff',
+     headerTitleStyle: {
+          fontWeight: 'bold'
+     }
+}
+
+export default App;
---
+++ Projects/rncrud/src/views/UserList.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { View, Text, FlatList, Alert } from 'react-native';
+import { ListItem, Avatar, Button, Icon } from 'react-native-elements';
+import users from '../data/users'
+
+const UserList = props => {
+
+     function getActions(user){
+          return (
+               <>
+                    <Button
+                         onPress={() => props.navigation.navigate('UserForm', user)}
+                         type='clear'
+                         icon={<Icon name='edit' size={25} color='oragne'/>}
+                    />
+                    <Button
+                         onPress={() => props.navigation.navigate('UserForm', user)}
+                         type='clear'
+                         icon={<Icon name='delete' size={25} color='red'/>}
+                    />
+               </>
+          )
+     }
+     function confirmUserDeletetion(user){
+          Alert.alert('Excluir usuario','Deseja exluir o usuario?',[
+               {
+                    text: 'Sim'
+               },
+                    text: 'Não'
+               }
+          ])
+     }
+
+     function getUserItem({ item: user }) {
+          return (
+               <ListItem 
+                    key={user.id} 
+                    bottomDivider 
+                    onPress={() => props.navigation.navigate('UserForm', user)}
+               >
+                    <Avatar source={{uri: user.avatarUrl}}/>
+                    <ListItem.Content>
+                         <ListItem.Title>{user.name}</ListItem.Title>
+                         <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                         <ListItem.Swipeable onPress={() => getActions(user)}>
+                              rightContent={
+                                   <Button
+                                        title="Delete"
+                                        icon={{ name: 'delete', color: 'white' }}
+                                        buttonStyle={{ minHeight: '100%', backgroundColor: 'red' }}
+                                        
+                                   />
+                              }
+                         </ListItem.Swipeable>
+                    </ListItem.Content>
+               </ListItem>
+          )
+     }
+
+     return (
+          <View>
+               <FlatList
+                    keyExtractor={user => user.id.toString()}
+                    data={users}
+                    renderItem={getUserItem}
+               />
+          </View>
+     );
+}
+
+export default UserList;
